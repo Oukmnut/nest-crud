d0a1b72f313a81eb139d0651ae297310f1f9b0ed
unknown <devmnut@gmail.com>
2025-07-04 11:37:13 +0700

    update message

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -5,30 +5,56 @@ import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  async create(@Body() createProductDto: CreateProductDto) {
+    const data = await this.productsService.create(createProductDto);
+    return {
+      success: true,
+      message: 'Product created successfully',
+      data: data,
+    };
   }
+
 
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  async findAll() {
+    const data = await this.productsService.findAll();
+    return {
+      success: true,
+      message: 'Products fetched successfully',
+      data,
+    };
   }
 
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const data = await this.productsService.findOne(+id);
+    return {
+      success: true,
+      message: 'Product fetched successfully',
+      data,
+    };
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  async update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    const data = await this.productsService.update(+id, updateProductDto);
+    return {
+      success: true,
+      message: 'Product updated successfully',
+      data,
+    };
+  }
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    await this.productsService.remove(+id);
+    return {
+      success: true,
+      message: 'Product deleted successfully',
+    };
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
-  }
 }
